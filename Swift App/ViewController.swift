c07c735fb54f78c89e710567a4ccfab6e39e3f9c
Rayan <TheKirzo93@gmail.com>
2017-06-18 13:23:48 +0200

    First update

--- Swift App/ViewController.swift
+++ Swift App/ViewController.swift
@@ -10,9 +10,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var TheLabel: UILabel!
+    
+    @IBOutlet weak var coolLabel: UILabel!
+
+    @IBAction func buttonTapped(_ sender: AnyObject) {
+        // code that runs whenever you tap the button
+        print("ok, buttonTapped is called")
+        TheLabel.text = "Bonjour !"
+    }
+    
+    @IBAction func coolbutton(_ sender: AnyObject) {
+        coolLabel.text = "Buttons are cool !"
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+
     }
 
     override func didReceiveMemoryWarning() {
